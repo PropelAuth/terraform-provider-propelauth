e11f38eaa0f1dca4eb57bab1b30c8989d90e03d6
Itai Levi <italev46@gmail.com>
2024-08-21 10:32:54 -0400

    fix(tf): comment out env variables

--- examples/resources/project_info/main.tf
+++ examples/resources/project_info/main.tf
@@ -7,9 +7,9 @@ terraform {
 }
 
 provider "propelauth" {
-  tenant_id  = "e1dc8461-5d8a-4bad-a929-19745de693f4"                                                             # or PROPELAUTH_TENANT_ID environment variable
-  project_id = "5a5f7a4f-1a51-4312-bbbe-4126cceab59b"                                                             # or PROPELAUTH_PROJECT_ID environment variable
-  api_key    = "c557308180b7da18d7e0e9cbd2ae3b36833c0165b5158c439efe59662df01701c2e23b00211b9c25b5223e51417f323b" # or PROPELAUTH_API_KEY environment variable
+  #   tenant_id  = "<PROPELAUTH_TENANT_ID>"  # or PROPELAUTH_TENANT_ID environment variable
+  #   project_id = "<PROPELAUTH_PROJECT_ID>" # or PROPELAUTH_PROJECT_ID environment variable
+  #   api_key    = "<PROPELAUTH_API_KEY>"    # or PROPELAUTH_API_KEY environment variable
 }
 
 resource "propelauth_project_info" "my_project_info" {
