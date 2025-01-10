a771a2665b49140b39bde57442d25f2cdd29316f
Matthew Mauer <mrmauer112@gmail.com>
2025-01-10 17:16:10 -0500

    Revert "update state from localhost application_url correctly"
    This reverts commit 83ea6daba4b3a4eb2478a1db93573bd2b84d3d9f.

--- internal/provider/fe_integration_resource.go
+++ internal/provider/fe_integration_resource.go
@@ -310,12 +310,7 @@ func convertPlanToUpdate(plan *feIntegrationResourceModel) propelauth.FeIntegrat
 }
 
 func updateStateForTestEnvironment(state *feIntegrationResourceModel, feIntegrationInfo *propelauth.TestFeIntegrationInfo) {
-	switch feIntegrationInfo.TestEnvFeIntegrationApplicationUrl.Type {
-	case "Localhost":
-		state.ApplicationUrl = types.StringValue(fmt.Sprintf("http://localhost:%d", feIntegrationInfo.TestEnvFeIntegrationApplicationUrl.Port))
-	case "SchemeAndDomain":
-		state.ApplicationUrl = types.StringValue(feIntegrationInfo.TestEnvFeIntegrationApplicationUrl.ApplicationUrl)
-	}
+	state.ApplicationUrl = types.StringValue(feIntegrationInfo.TestEnvFeIntegrationApplicationUrl.ApplicationUrl)
 	state.LoginRedirectPath = types.StringValue(feIntegrationInfo.LoginRedirectPath)
 	state.LogoutRedirectPath = types.StringValue(feIntegrationInfo.LogoutRedirectPath)
 
