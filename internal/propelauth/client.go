593292bb6387ccda1f9d207b87117d0da6b4f30c
Matthew Mauer <mrmauer112@gmail.com>
2026-07-15 11:27:05 -0400

    tell linter to ignore dumb cases...

--- internal/propelauth/client.go
+++ internal/propelauth/client.go
@@ -117,7 +117,7 @@ func (c *PropelAuthClient) requestHelper(method string, url string, body []byte)
 	if err != nil {
 		return nil, fmt.Errorf("error making http request: %w", err)
 	}
-	defer resp.Body.Close()
+	defer resp.Body.Close() //nolint:errcheck
 
 	// convert the response body to a stream of bytes
 	buf := new(bytes.Buffer)
